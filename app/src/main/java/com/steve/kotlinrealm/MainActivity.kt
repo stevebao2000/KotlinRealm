53626b8c7a1502f401310c8eab2bffb33637c81a
Steve Bao <34757708+stevebao2000@users.noreply.github.com>
2021-06-04 18:02:04 -0700

    First Commit

---
+++ app/src/main/java/com/steve/kotlinrealm/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.steve.kotlinrealm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickToTestRealm(view: View) {}
+}
